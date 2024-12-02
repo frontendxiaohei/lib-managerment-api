1f65cea2ea4c9a954cf1005164165102afd842c3
big whiter <38613864+frontendxiaohei@users.noreply.github.com>
2024-12-02 15:56:30 +0800

    feat: book 模块1

---
+++ src/book/book.service.ts
@@ -0,0 +1,27 @@
+import { CreateBookDto } from './dto/create-book.dto';
+import { Injectable } from '@nestjs/common';
+import { UpdateBookDto } from './dto/update-book.dto';
+
+@Injectable()
+export class BookService {
+
+    list() {
+
+    }
+
+    findById(id: number) {
+
+    }
+
+    create(createBookDto: CreateBookDto) {
+
+    }
+
+    update(updateBookDto: UpdateBookDto) {
+
+    }
+
+    delete(id: number) {
+        
+    }
+}
